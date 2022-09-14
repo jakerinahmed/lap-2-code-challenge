93c86c834c43714875ded1c39fc1e6fb918d26d2
Stefan Mircea <stefancmircea@gmail.com>
2022-09-14 12:14:59 +0100

    add route to create posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsController = require('../controllers/posts')
 
 router.get('/', postsController.index);
+router.post('/', postsController.create);
 
 module.exports = router;
